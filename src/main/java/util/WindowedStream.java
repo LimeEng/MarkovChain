a1c4c5344e4dc887eb52125f4a0960e735067507
Emil Englesson <englesson.emil@gmail.com>
2018-05-28 21:19:24 +0200

    Added some documentation
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- src/main/java/util/WindowedStream.java
+++ src/main/java/util/WindowedStream.java
@@ -7,6 +7,16 @@ import java.util.stream.StreamSupport;
 
 public class WindowedStream {
 
+    /**
+     * Returns a sliding window in the form of a Stream of lists. The window
+     * size specify the size of the lists
+     *
+     * @param source
+     *            the original stream which will be partitioned into windows
+     * @param windowSize
+     *            the size of each window
+     * @return a stream of equally sized windows
+     */
     public static <T> Stream<List<T>> windowed(Stream<T> source, int windowSize) {
         Spliterator<List<T>> spliterator = new WindowSpliterator<>(source.spliterator(), windowSize);
         return StreamSupport.stream(spliterator, false)
